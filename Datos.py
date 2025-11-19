87c96bedd8dc07813703a67d1d8c1035cc7cab30
Giovanny28 <giovannypn28@gmail.com>
2025-11-19 08:14:28 -0600

    Create Datos.py

---
+++ Datos.py
@@ -0,0 +1,7 @@
+# Solicitar datos al usuario
+nombre = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+carrera = input("Ingresa tu carrera: ")
+
+# Mostrar mensaje
+print(f"Hola {nombre}, estudias {carrera} y tienes {edad} años.")
